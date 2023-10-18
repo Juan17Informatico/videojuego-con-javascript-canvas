fe80673d972c55eb1f17e072fd1b507b8ba43985
Juan17Informatico <juancampuzano2356@gmail.com>
2023-10-18 10:37:21 -0500

    Clase 7. Se refactorizo el codigo para hacerlo mas legible y entendible a la hora de leerlo. En lugar de dos ciclo for se realizo la renderizacion del mapa con el metodo de arrays foreach

--- game.js
+++ game.js
@@ -12,12 +12,16 @@ window.addEventListener('resize', setCanvasSize);
  * Función para establecer el tamaño del canvas según el tamaño de la pantalla
  */
 function setCanvasSize(){
+
   if (window.innerHeight > window.innerWidth) {
+    // Para dispositivos moviles
     canvasSize = window.innerWidth * 0.75;
   } else {
+    // Para computadoras
     canvasSize = window.innerHeight * 0.75;
   }
   
+  //Se establece el tamaño del canvas según el tamaño de la pantalla
   canvas.setAttribute('width', canvasSize);
   canvas.setAttribute('height', canvasSize);
   
@@ -31,44 +35,39 @@ function setCanvasSize(){
  * Función para iniciar el juego
  **/
 function startGame() {
-    // console.log({ canvasSize, elementsSize });
-  
-    // console.log({game});
-
-    //La variable game es el contexto del canvas
-    // Se establece el tamaño de los elementos del canvas
+    // Se establece el tamaño de los elementos del texto del canvas 
     game.font = (elementsSize - 10) + 'px Verdana';
     game.textAlign = 'end';
 
-    // console.log({game});
-
     //Trim es una función que sirve para eliminar los espacios en blanco al inicio y al final de un string
     //Split es una función que sirve para separar un string en un array de strings
-    //Se establece el mapa del juego 
+    
+    //Configuración del mapa del juego
     const mapa = maps[0];
-    //Se crea una variable para almacenar el mapa en un array de strings, eliminando los espacios al inicio y al final
     const mapaRows = mapa.trim().split('\n');
-    //Se crea una variable para almacenar el mapa en un array de arrays, eliminando los espacios al inicio y al final y separando cada string en un array
     const mapaColsRows = mapaRows.map(row => row.trim().split(''));
-    console.log({mapaColsRows});
 
+    //Se realiza el render del mapa mediante un ForEach. 
+    mapaColsRows.forEach((row, rowI) => { //element, index
+      row.forEach((col, colI)=> { //element, index
+        const emoji = emojis[col];
+        const x = elementsSize * (colI + 1);
+        const y = (elementsSize * (rowI + 1)) - 15;
+        game.fillText(emoji, x, y);
+      })
+    })
+
+    /** 
+     * Forma alternativa de hacerlo. 
+     */
     //Se establece la posición de los elementos del canvas según el tamaño de los elementos realizando un ciclo for para llenar cada cuadricula
-    for (let row = 1; row <= 10; row++) {
-      for (let col = 1; col <= 10; col++) {
-          game.fillText(emojis[mapaColsRows[row - 1][col- 1]], elementsSize * col, (elementsSize * row) - 15);        
-      }
-    }
-
-    // window.innerHeight
-    // window.innerWidth
-    // game.fillRect(100, 25, 100, 100);
-    // game.clearRect(105, 30, 90, 87);
-
-
-    // game.fillStyle = 'blue';
-    // game.font = '25px Arial';
-    // game.textAlign = 'start';
-    // game.fillText('Platzi', 120, 85);
-    // //Pone el texto "Platzi" en negrilla 
-    // game.strokeText('Platzi', 100, 100);
+    // for (let row = 1; row <= 10; row++) {
+    //   for (let col = 1; col <= 10; col++) {
+    //       const emoji = emojis[mapaColsRows[row - 1][col -1]];
+    //       const x = elementsSize * col;
+    //       const y = (elementsSize * row) - 15;
+    //       game.fillText(emoji, x, y);        
+    //   }
+    // }
 }
+
